da3d0c68fa3b40a7d7ce749d74e6574bfb02ac9d
MichelleCantu26 <michellealex738@gmail.com>
2025-06-13 12:02:17 -0500

    Se arregla el link en transfer.js que estaba el de pruebas

--- src/app/transferStocks/transfer.js
+++ src/app/transferStocks/transfer.js
@@ -90,7 +90,7 @@ const StockTransfer = () => {
     }
     setLoading(true);
     try {
-      const response = await fetch("https://pruebas-sap-back.onrender.com/stock-transfer", {
+      const response = await fetch("https://biocells-sap-test.onrender.com/stock-transfer", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
@@ -263,7 +263,7 @@ const removeLastItemLine = () => {
    useEffect(() => {
     const fetchItems = async () => {
       try {
-        const response = await fetch("https://pruebas-sap-back.onrender.com/items");
+        const response = await fetch("https://biocells-sap-test.onrender.com/items");
         const data = await response.json();
         setItems(data); // Asume que data contiene la lista de ítems
       } catch (error) {
@@ -287,7 +287,7 @@ const removeLastItemLine = () => {
 
   const fetchBatchNumbers = async (itemCode) => {
     try {
-      const response = await fetch(`https://pruebas-sap-back.onrender.com/inventario?codigo_item=${itemCode}&codigo_almacen=${origen}`);
+      const response = await fetch(`https://biocells-sap-test.onrender.com/inventario?codigo_item=${itemCode}&codigo_almacen=${origen}`);
       const text = await response.text();
       const data = JSON.parse(text);
   
@@ -319,7 +319,7 @@ const removeLastItemLine = () => {
   useEffect(() => {
     const fetchWarehouses = async () => {
       try {
-        const response = await fetch("https://pruebas-sap-back.onrender.com/get-warehouses");
+        const response = await fetch("https://biocells-sap-test.onrender.com/get-warehouses");
         const data = await response.json();
         setWarehouses(data.warehouses); // Asumiendo que el backend devuelve la lista de bodegas
       } catch (error) {
@@ -338,7 +338,7 @@ useEffect(() => {
     const signal = controller.signal;
 
     try {
-      const response = await fetch(`https://pruebas-sap-back.onrender.com/business-partner-price-list/${cliente}`, { signal });
+      const response = await fetch(`https://biocells-sap-test.onrender.com/business-partner-price-list/${cliente}`, { signal });
 
       if (!response.ok) {
         throw new Error(`Error HTTP: ${response.status} - ${response.statusText}`);
